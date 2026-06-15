a7b06f9a35133a63b57ed14755c4b892b2c69859
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2026-06-15 16:43:58 -0700

    predict function now works

--- ocr.py
+++ ocr.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class NeuralNetwork:
     def _rand_initialize_weights(self, size_in, size_out):
@@ -14,9 +15,14 @@ class NeuralNetwork:
         self.input_layer_bias = self._rand_initialize_weights(1, num_hidden_nodes)
         self.hidden_layer_bias = self._rand_initialize_weights(1, 10)
     
+    def sigmoid(self, matrix):
+        sigmoid_to_matrix = np.vectorize(self._sigmoid_scalar)
+        new_matrix = sigmoid_to_matrix(matrix)
+        return new_matrix
+
     def forward_propogate(self):
-        y1 = np.dot(np.mat(self.theta1), np.map(data['y0']).T)
-        sum1 = y1 + np.mat(self.input_layer_bias)
+        y1 = np.dot(np.asmatrix(self.theta1), np.map(data['y0']).T)
+        sum1 = y1 + np.asmatrix(self.input_layer_bias)
         y1 = self.sigmoid(sum1)
     
         y2 = np.dot(np.array(self.theta2), y1)
@@ -26,22 +32,26 @@ class NeuralNetwork:
     def back_propogate(self):
         actual_vals = [0] * 10
         actual_vals[data['label']] = 1
-        output_errors = np.mat(actual_vals).T - np.mat(y2)
-        hidden_errors = np.multiply(np.dot(np.mat(self.theta2).T, output_errors), self.sigmoid_prime(sum1))
+        output_errors = np.asmatrix(actual_vals).T - np.asmatrix(y2)
+        hidden_errors = np.multiply(np.dot(np.asmatrix(self.theta2).T, output_errors), self.sigmoid_prime(sum1))
     
-        self.theta1 += self.LEARNING_RATE * np.dot(np.mat(hidden_errors), np.mat(data['y0']))
-        self.theta2 += self.LEARNING_RATE * np.dot(np.mat(output_errors), np.mat(y1).T)
+        self.theta1 += self.LEARNING_RATE * np.dot(np.asmatrix(hidden_errors), np.asmatrix(data['y0']))
+        self.theta2 += self.LEARNING_RATE * np.dot(np.asmatrix(output_errors), np.asmatrix(y1).T)
         self.hidden_layer_bias += self.LEARNING_RATE * output_errors
         self.input_layer_bias += self.LEARNING_RATE * hidden_errors
     
     def predict(self, test):
-        y1 = np.dot(np.mat(self.theta1), np.mat(test).T)
-        y1 = y1 + np.mat(self.input_layer_bias)
+        y1 = np.dot(np.asmatrix(self.theta1), np.asmatrix(test).T)
+        y1 = y1 + np.asmatrix(self.input_layer_bias)
         y1 = self.sigmoid(y1)
     
         y2 = np.dot(np.array(self.theta2), y1)
-        y2 = y2 + np.mat(self.hidden_layer_bias)
+        y2 = y2 + np.asmatrix(self.hidden_layer_bias)
         y2 = self.sigmoid(y2)
     
         results = y2.T.tolist()[0]
+        highest_confidence = max(results)
+        confidence_percent = int(highest_confidence * 100)
+        prediction = results.index(highest_confidence)
+        print("Predicting digit is {0} with {1}% confidence".format(prediction, confidence_percent))
         return results.index(max(results))
